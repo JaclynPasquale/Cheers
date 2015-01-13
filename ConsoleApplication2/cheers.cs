ed55a8b8788348ceeb0e3c611cc9519eda99229c
JaclynPasquale <jaclynpasquale@gmail.com>
2015-01-13 13:34:22 -0600

    set-up commit

---
+++ ConsoleApplication2/cheers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("what is your name?");
+            System.Console.ReadKey();
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Welcome, " + name);
+            string nameUpper = name.ToUpper();
+            System.Console.WriteLine(name + " is... GRAND!");
+
+            ////foreach (char letter in name.ToLower())
+            //{
+            //    string article = "a";
+            //    if ("halfnorsemix")
+
+            ////}
+
+            
+
+        }
+    }
+}
